2d02b5e41a45a55bc0113713a7c8c9b08e3fc026
Oliver Lomanat <lomanatoliver@gmail.com>
2025-07-22 08:25:49 +0300

    Adds the first part of the form

--- src/Pages/MealForm.js
+++ src/Pages/MealForm.js
@@ -76,6 +76,7 @@ function MealForm() {
     return (
         <div>
             <form onSubmit={handleSubmit}>
+                {/* First part of the form thats the ItemName and Calories */}
                 <div>
                     <input
                     type="text"
@@ -83,13 +84,34 @@ function MealForm() {
                     value={currentNutrition.itemName}
                     onChange={handleChange}                  
                     />
+
                     <input
-                    type="text"
-                    name="itemName"
-                    value={currentNutrition.itemName}
+                    type="number"
+                    name="calories"
+                    value={currentNutrition.calories}
                     onChange={handleChange}
                     />
                 </div>
+
+                {/* Second part of the form thats the Protein and Carbs */}
+
+                <div>
+                    <input
+                    type="number"
+                    name="protein"
+                    value={currentNutrition.protein}
+                    onChange={handleChange}
+                    />
+                    <input
+                    type="number"
+                    name="carbs"
+                    value={currentNutrition.carbs}
+                    onChange={handleChange}
+                    />
+                </div>
+
+                {/* Third part of the form thats the Protein and Carbs */}
+                
             </form>
         </div>
     )
