ba5e2e3dfa36e2963712b4684d6b3bbd1c3eb2fc
Oliver Lomanat <lomanatoliver@gmail.com>
2025-07-22 08:38:53 +0300

    Adds data to App.js from MealForm.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react";
+import MealForm from "./Pages/MealForm";
 
 function App() {
+      //Majeor form states
+    const [nutritions, setNutrition] = useState([]);
+    const [currentNutrition, setCurrentNutrition] = useState({
+        itemName: "",
+        calories: null,
+        protein: null,
+        carbs: null,
+        fat: null,
+    })
+
+    //Function for handling the input onChange event
+    function handleChange(e) {
+        setCurrentNutrition(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+     function handleSubmit(e) {
+        e.preventDefault();
+        const newNutrition = {
+            ...currentNutrition,
+            itemName: currentNutrition.itemName,
+            calories: currentNutrition.calories,
+            protein: currentNutrition.protein,
+            carbs: currentNutrition.carbs,
+            fat: currentNutrition.fat,
+        }
+
+        fetch('http://localhost:4000/nutritions', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newNutrition),
+        })
+            .then(res => {
+                if(!res.ok) throw new Error("Failed to add nutrition");
+                return res.json()
+            })
+            .then(data => setNutrition([...nutritions, data]))
+            .catch(error => console.error("POST error:", error));
+
+
+        setCurrentNutrition({
+        itemName: "",
+        calories: null,
+        protein: null,
+        carbs: null,
+        fat: null,
+        })
+    }
+
+    function deleteNutrition(id) {
+        fetch(`http://localhost:4000/${id}`,{
+            method: "DELETE",
+        })
+        .then(res => {
+            if(!res.ok) throw new Error("Failed to delete");
+            setNutrition(nutritions.filter(nutrition => nutrition.id !== id ))
+        })
+        .catch(error => console.error("DELETE error:", error))
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:4000/nutritions')
+            .then(res => {
+                if(!res.ok) throw new Error("Failed to fetch");
+                return res.json();
+            })
+            .then(data => setNutrition(data))
+            .catch(error => console.error("Fetch error:", error))
+    }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
--- src/Pages/MealForm.js
+++ src/Pages/MealForm.js
@@ -1,77 +1,6 @@
 const { useState, useEffect } = require("react");
 
-function MealForm() {
-    //Majeor form states
-    const [nutritions, setNutrition] = useState([]);
-    const [currentNutrition, setCurrentNutrition] = useState({
-        itemName: "",
-        calories: null,
-        protein: null,
-        carbs: null,
-        fat: null,
-    })
-
-    //Function for handling the input onChange event
-    function handleChange(e) {
-        setCurrentNutrition(prev => ({
-            ...prev,
-            [e.target.name]: e.target.value
-        }))
-    }
-
-     function handleSubmit(e) {
-        e.preventDefault();
-        const newNutrition = {
-            ...currentNutrition,
-            itemName: currentNutrition.itemName,
-            calories: currentNutrition.calories,
-            protein: currentNutrition.protein,
-            carbs: currentNutrition.carbs,
-            fat: currentNutrition.fat,
-        }
-
-        fetch('http://localhost:4000/nutritions', {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(newNutrition),
-        })
-            .then(res => {
-                if(!res.ok) throw new Error("Failed to add nutrition");
-                return res.json()
-            })
-            .then(data => setNutrition([...nutritions, data]))
-            .catch(error => console.error("POST error:", error));
-
-
-        setCurrentNutrition({
-        itemName: "",
-        calories: null,
-        protein: null,
-        carbs: null,
-        fat: null,
-        })
-    }
-
-    function deleteNutrition(id) {
-        fetch(`http://localhost:4000/${id}`,{
-            method: "DELETE",
-        })
-        .then(res => {
-            if(!res.ok) throw new Error("Failed to delete");
-            setNutrition(nutritions.filter(nutrition => nutrition.id !== id ))
-        })
-        .catch(error => console.error("DELETE error:", error))
-    }
-
-    useEffect(() => {
-        fetch('http://localhost:4000/nutritions')
-            .then(res => {
-                if(!res.ok) throw new Error("Failed to fetch");
-                return res.json();
-            })
-            .then(data => setNutrition(data))
-            .catch(error => console.error("Fetch error:", error))
-    }, []);
+export default function MealForm() {
 
     return (
         <div>
@@ -118,6 +47,8 @@ function MealForm() {
                     value={currentNutrition.fat}
                     onChange={handleChange}
                     />
+
+                    <button>Add Item</button>
                                 
             </form>
         </div>
