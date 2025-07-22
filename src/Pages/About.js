134c134e3bd7078bf0c6d0827328b3b417d9a1db
Oliver Lomanat <lomanatoliver@gmail.com>
2025-07-22 15:05:12 +0300

    Adds the head tag

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 export default function About() {
     return (
         <div className="max-w-3xl mx-auto px-4 py-8 text-center">
-            
+            <h1 className="text-3xl md:text-4xl font-bold text-white mb-8">
+               About <span className="text-blue-300">NeuTrackeR</span>
+           </h1>
+          
+
         </div>
     );
 }
