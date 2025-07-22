98475aced06d345a669d91b82e67c851e8b50e80
Oliver Lomanat <lomanatoliver@gmail.com>
2025-07-22 07:25:39 +0300

    Adding a comment

--- src/Pages/MealForm.js
+++ src/Pages/MealForm.js
@@ -1,6 +1,7 @@
 const { useState } = require("react");
 
 function MealForm() {
+    //Majeor form states
     const [nutrition, setNutrition] = useState([]);
     const [currentNutrition, setCurrentNutrition] = useState({
         itemName: "",
