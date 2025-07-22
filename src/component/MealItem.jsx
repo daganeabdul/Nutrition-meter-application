43c6caf69ba6766402263ad64cd82f8147acefa2
Neathan Kungu <natekungu857@gmail.com>
2025-07-22 20:31:27 +0300

    cards styled

--- src/component/MealItem.jsx
+++ src/component/MealItem.jsx
@@ -4,14 +4,16 @@ import React from "react";
 function MealItem({nutritions, onDelete, onEdit}) {
 
 return(
-    <div className="meal-item">
+    <div className="card">
         <h3>{nutritions.itemName}</h3>
         <p>Calories: {nutritions.calories}</p>
         <p>Protein: {nutritions.protein}g</p>
         <p>Carbs: {nutritions.carbs}g</p>
         <p>Fat: {nutritions.fat}g</p>
-        <button onClick={onDelete}>Delete</button>
-        <button onClick={onEdit}>Edit</button>
+        <div className="card-buttons">
+        <button onClick={onDelete} className="delete-btn">Delete</button>
+        <button onClick={onEdit} className="edit-btn">Edit</button>
+        </div>
     </div>
 )
 }
