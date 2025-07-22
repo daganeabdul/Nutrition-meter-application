1af43a1d03140f503ae25290767d793118141f36
Oliver Lomanat <lomanatoliver@gmail.com>
2025-07-22 14:45:14 +0300

    Adds head tag and style

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,9 @@
 export default function Home() {
     return (
         <div className="text-center">
-           
+           <h1 className="text-4xl font-bold mb-6 text-blue-300">
+            Welcome to <span className="text-white">NeuTrackeR</span>
+           </h1>
         </div>
     );
 }
