f2c63862877b54c7887c59c9bf82060468d78c24
Oliver Lomanat <lomanatoliver@gmail.com>
2025-07-22 14:19:51 +0300

    Adds style to the first and second part of form

--- src/Pages/MealForm.js
+++ src/Pages/MealForm.js
@@ -1,10 +1,12 @@
 export default function MealForm({ currentNutrition, handleChange, handleSubmit }) {
     return (
         <div className="max-w-md mx-auto bg-blue-700 rounded-lg shadow-lg">
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <div className="">
+            <form onSubmit={(e) => handleSubmit(e)} className="space-y-4">
+                <div className="grid grid-cols-2 gap-4">
+                    <div>
+                    <label className="block mb-1 text-white text-sm font-bold">Item Name</label>
                     <input
-                        className=""
+                        className="w-full px-3 py-2 rounded bg-slate-600 text-white"
                         type="text"
                         name="itemName"
                         value={currentNutrition.itemName}
@@ -12,7 +14,11 @@ export default function MealForm({ currentNutrition, handleChange, handleSubmit 
                         placeholder="Item Name"
                         required
                     />
+                    </div>
+                    <div>
+                    <label className="block mb-1 text-white text-sm font-bold">Calories</label>
                     <input
+                    className="w-full px-3 py-2 rounded bg-slate-600 text-white"
                         type="number"
                         name="calories"
                         value={currentNutrition.calories}
@@ -20,9 +26,13 @@ export default function MealForm({ currentNutrition, handleChange, handleSubmit 
                         placeholder="Calories"
                         required
                     />
+                    </div>
                 </div>
-                <div>
+                <div className="grid grid-cols-2 gap-4">
+                    <div>
+                    <label className="block text-white mb-1 text-sm font-bold">Protein (g)</label>
                     <input
+                        className="w-full px-3 py-2 rounded bg-blue-600 text-white"
                         type="number"
                         name="protein"
                         value={currentNutrition.protein}
@@ -30,7 +40,11 @@ export default function MealForm({ currentNutrition, handleChange, handleSubmit 
                         placeholder="Protein"
                         required
                     />
+                    </div>
+                    <div>
+                    <label className="block text-white mb-1 text-sm font-bold">Carbs</label>
                     <input
+                        className="w-full px-3 py-2 rounded bg-blue-600 text-white"
                         type="number"
                         name="carbs"
                         value={currentNutrition.carbs}
@@ -38,6 +52,7 @@ export default function MealForm({ currentNutrition, handleChange, handleSubmit 
                         placeholder="Carbs"
                         required
                     />
+                    </div>
                 </div>
                 <input
                     type="number"
